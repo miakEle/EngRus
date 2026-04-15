a572265f25906554c752e0f6cabbff4f2a330101
miakele <myakushkinaea@gmail.com>
2026-04-15 17:00:49 +0300

    in di usecases

--- app/src/main/java/com/example/engrus/domain/usecases/GetWordCardByIdUseCase.kt
+++ app/src/main/java/com/example/engrus/domain/usecases/GetWordCardByIdUseCase.kt
@@ -3,7 +3,7 @@ package com.example.engrus.domain.usecases
 import com.example.engrus.domain.repository.WordCardRepository
 import javax.inject.Inject
 
-class GetWordCardById @Inject constructor(private val wordCardRepository: WordCardRepository) {
+class GetWordCardByIdUseCase @Inject constructor(private val wordCardRepository: WordCardRepository) {
 
     operator fun invoke(wordCardId: Long){
         wordCardRepository.getWordCardById(wordCardId)
--- app/src/main/java/com/example/engrus/domain/usecases/GetWordCardsListUseCase.kt
+++ app/src/main/java/com/example/engrus/domain/usecases/GetWordCardsListUseCase.kt
@@ -5,7 +5,7 @@ import com.example.engrus.domain.repository.WordCardRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetWordCardsList @Inject constructor(private val wordCardRepository: WordCardRepository) {
+class GetWordCardsListUseCase @Inject constructor(private val wordCardRepository: WordCardRepository) {
 
     operator fun invoke(): Flow<List<WordCard>>{
         return wordCardRepository.getWordCardsList()
