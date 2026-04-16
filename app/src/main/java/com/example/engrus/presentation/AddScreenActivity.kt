5cb1fcbf500ea2c934d0c42c9c5e2c328a1dc24b
miakele <myakushkinaea@gmail.com>
2026-04-16 15:12:17 +0300

    AddScreenActivity.kt added

--- app/src/main/java/com/example/engrus/presentation/AddScreenActivity.kt
+++ app/src/main/java/com/example/engrus/presentation/AddScreenActivity.kt
@@ -1,24 +1,21 @@
 package com.example.engrus.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.engrus.databinding.ActivityAddScreenBinding
 import com.example.engrus.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class AddScreenActivity: AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityAddScreenBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAddScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.floatingActionButton.setOnClickListener {
-            val intent = Intent(this, AddScreenActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
